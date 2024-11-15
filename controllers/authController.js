3997b30c3fc32666cd6e5ae0019555e5c5dc441c
Lionel <lpei1900@gmail.com>
2024-11-15 09:13:49 -0500

    Fixed issues with user sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
+const pool = require('../db/pool');
 
 exports.getLoginPage = (req, res) => {
   res.render('auth/login');
@@ -29,7 +30,7 @@ exports.handleLogout = (req, res, next) => {
 };
 
 exports.createUser = async (req, res, next) => {
-  const { firstName, lastName, password, confirmPassword, username } = req.body;
+  const { first_name, last_name, username, password, confirmPassword } = req.body;
 
   if (password !== confirmPassword) {
       //RENDER ERROR HERE
@@ -39,7 +40,7 @@ exports.createUser = async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     await pool.query('INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)', [
-      firstName, lastName, username, hashedPassword
+      first_name, last_name, username, hashedPassword
     ]);
 
     res.redirect('/messagePage');
