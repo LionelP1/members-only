6b972dc6734bd4347d39cff40232cf9b053c7d33
Lionel <lpei1900@gmail.com>
2024-11-15 22:49:40 -0500

    Implemented the queries for creating and retrieving all messages

---
+++ db/queries.js
@@ -0,0 +1,22 @@
+const pool = require('./pools');
+
+async function getMessages() {
+  const query = `
+  SELECT users.username, messages.title, messages.content, messages.created_at
+  FROM users
+  JOIN messages ON users.id = messages.user_id
+  ORDER BY messages.id DESC;
+  `;
+  const { rows } = await pool.query(query);
+  return rows;
+}
+
+async function addMessage(messageDetails) {
+  const { user_id, title, content} = messageDetails;
+  await pool.query('INSERT INTO messages (user_id, title, content) VALUES ($1, $2, $3)', [user_id, title, content]);
+}
+
+module.exports = {
+  getMessages,
+  addMessage,
+};
