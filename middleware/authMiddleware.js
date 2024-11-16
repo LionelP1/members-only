6d32e8d2d6a46a60a7a0a5e0f300fd3b44765db1
Lionel <lpei1900@gmail.com>
2024-11-15 20:30:59 -0500

    Implemented the middleware to redirect user if logged in

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,3 +4,10 @@ exports.isAuthenticated = (req, res, next) => {
   }
   res.redirect('/login');
 };
+
+exports.redirectIfAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+      return res.redirect('/messages');
+  }
+  next();
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const authController = require('../controllers/authController'); // Ensure this points to your controller file
+const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/login', authController.getLoginPage);
+router.get('/login', authMiddleware.redirectIfAuthenticated, authController.getLoginPage);
 router.post('/login', authController.handleLogin);
 
-router.get('/signup', authController.getSignupPage);
+router.get('/signup', authMiddleware.redirectIfAuthenticated, authController.getSignupPage);
 router.post('/signup', authController.createUser);
 
 router.post('/logout', authController.handleLogout);
