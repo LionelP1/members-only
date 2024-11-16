5494baeef2f1cd95e0ba2e8bcc21398abd4ee989
Lionel <lpei1900@gmail.com>
2024-11-15 23:46:11 -0500

    Implemented other routes for messages

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -4,6 +4,13 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/messages', authMiddleware.isAuthenticated, messageController.getMessagePage);
-router.get('/', messageController.getHomePage)
+router.get('/', messageController.getHomePage);
+
+router.get('/messages/new', (req, res) => {
+    res.render('form');
+});
+
+router.post('/messages/new', messageController.createNewMessage);
+
 
 module.exports = router;
