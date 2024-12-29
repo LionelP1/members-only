6e88fca6b69def2644245a4141223aca48d78b5e
Lionel <lpei1900@gmail.com>
2024-12-29 18:53:04 -0500

    Updated to use prisma

--- config/passport.js
+++ config/passport.js
@@ -1,23 +1,22 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
-const pool = require("../db/pool");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 passport.use(new LocalStrategy(
   async (username, password, done) => {
     try {
-      const { rows } = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-      const user = rows[0];
-
+      const user = await prisma.user.findUnique({
+        where: { username: username }
+      });
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
         return done(null, false, { message: 'Incorrect password.' });
       }
-
       return done(null, user);
     } catch (error) {
       return done(error);
@@ -31,8 +30,9 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-    const user = rows[0];
+    const user = await prisma.user.findUnique({
+      where: { id: id }
+    });
 
     if (!user) {
       return done(new Error('User not found'));
