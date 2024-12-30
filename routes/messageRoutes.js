f2ba089158faeb163524a58f2f2779bf7ccf03a8
Lionel <lpei1900@gmail.com>
2024-12-29 20:06:24 -0500

    Updated project

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const messageController = require('../controllers/messageController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
+const authController = require('../controllers/authController');
 
 router.get('/messages', authMiddleware.isAuthenticated, messageController.getMessagePage);
 router.get('/', messageController.getHomePage);
