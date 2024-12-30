8ce00ce9b08772feb75ff2a83fdedc365e70de62
Lionel <lpei1900@gmail.com>
2024-12-29 19:01:33 -0500

    Updated session

--- config/session.js
+++ config/session.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 
 const expressSession = require('express-session');
-const pgSession = require('connect-pg-simple')(expressSession);
-const pool = require('../db/pool');
+const PrismaSessionStore = require('@quixo3/prisma-session-store').PrismaSessionStore;
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 module.exports = expressSession({
-    store: new pgSession({
-        pool: pool,
-        tableName: 'session',
-    }),
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+  store: new PrismaSessionStore(prisma, {
+    checkPeriod: 2 * 60 * 1000,
+    dbRecordIdIsSessionId: true,
+    sessionDataFieldName: 'data',
+  }),
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
 });
